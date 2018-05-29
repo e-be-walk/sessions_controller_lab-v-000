a5b35b3e4d97f1f5588ce238d63840d0740d0fd5
Erin Walker <erinb0317@hotmail.com>
2018-05-29 00:24:50 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,12 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create 
-  end 
+  def create
+    session[:username] = params[:username]
+    redirect_to '/'
+  end
 
   def destroy
+    session.delete :username
   end 
 end
